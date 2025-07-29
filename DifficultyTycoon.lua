5d2f8003d3eb60562747074ec89f93948fd7b234
lucasr125 <lucasr125348@gmail.com>
2025-07-29 18:16:54 -0300

    Updated

--- DifficultyTycoon.lua
+++ DifficultyTycoon.lua
@@ -4,102 +4,89 @@ local playerTycoon = nil
 local playerCash = localPlayer:WaitForChild("leaderstats").Cash
 local gameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
 
-local autoClickerCooldown = 0.1
-local autoBuyTycoonButtonsCooldown = 0.1
-local autoCollectCashCooldown = 0.1
-local autoClickerActive = false
-local autoBuyTycoonButtonsActive = false
-local autoCollectCashActive = false
+local settings = {
+    autoClickerCooldown = 0.01, 
+    autoBuyTycoonButtonsCooldown = 1,
+    autoCollectCashCooldown = 0.5,
+
+    autoCollectCash = false,
+    autoBuyTycoonButtons = false,
+    autoMineClicker = false,
+}
 
 local function getIqBadge()
-if firetouchinterest then
-firetouchinterest(playerHRP, workspace["Wall Hop"].asset:GetChildren()[2], 0)
-firetouchinterest(playerHRP, workspace["Wall Hop"].asset:GetChildren()[2], 1)
-end
+    if firetouchinterest then
+        firetouchinterest(playerHRP, workspace["Wall Hop"].asset:GetChildren()[2], 0)
+        firetouchinterest(playerHRP, workspace["Wall Hop"].asset:GetChildren()[2], 1)
+    end
 end
 
 local function getChairBadge()
-if firetouchinterest then
-firetouchinterest(playerHRP, workspace.Obby.Chair:GetChildren()[3], 0)
-firetouchinterest(playerHRP, workspace.Obby.Chair:GetChildren()[3], 1)
-firetouchinterest(playerHRP, workspace.Obby.TP.Part1, 0)
-firetouchinterest(playerHRP, workspace.Obby.TP.Part1, 1)
-end
+    if firetouchinterest then
+        firetouchinterest(playerHRP, workspace.Obby.Chair:GetChildren()[3], 0)
+        firetouchinterest(playerHRP, workspace.Obby.Chair:GetChildren()[3], 1)
+        task.wait()
+        firetouchinterest(playerHRP, workspace.Obby.TP.Part1, 0)
+        firetouchinterest(playerHRP, workspace.Obby.TP.Part1, 1)
+    end
 end
 
 local function explodeMango()
-if fireclickdetector then
-for i = 1, 30, 1 do
-fireclickdetector(workspace.Lobby.mango.Handle.ClickDetector)
-task.wait(0.15)
-end
-end
+    if fireclickdetector then
+        for i = 1, 30, 1 do
+            fireclickdetector(workspace.Lobby.mango.Handle.ClickDetector)
+            task.wait(0.15)
+        end
+    end
 end
 
 local function getNeaTBadge()
-if fireclickdetector and firetouchinterest then
-fireclickdetector(playerTycoon.Purchases.neat.Frame.tp.ClickDetector)
-task.wait()
-firetouchinterest(playerHRP, playerTycoon.Purchases.neat.WinPad3, 0)
-firetouchinterest(playerHRP, playerTycoon.Purchases.neat.WinPad3, 1)
-task.wait()
-if mouse1click then
-mouse1click(localPlayer.PlayerGui.exit.exitButton)
-else
-    Rayfield:Notify({
-    Title = "Warning",
-    Content = "Your executor doesnt supports 'mouse1click' function, you'll need click on 'Exit' button manually.",
-    Duration = 6.5,
-    Image = "rewind",
-})
-end
-end
+    if fireclickdetector and firetouchinterest then
+        fireclickdetector(playerTycoon.Purchases.neat.Frame.tp.ClickDetector)
+        task.wait()
+        firetouchinterest(playerHRP, playerTycoon.Purchases.neat.WinPad3, 0)
+        firetouchinterest(playerHRP, playerTycoon.Purchases.neat.WinPad3, 1)
+        task.wait()
+        if mouse1click then
+            mouse1click(localPlayer.PlayerGui.exit.exitButton)
+        else
+            Rayfield:Notify({Title = "Warning",Content = "Your executor doesnt supports 'mouse1click' function, you'll need click on 'Exit' button manually.",Duration = 6.5,Image = "rewind",})
+        end
+    end
 end
 
 local function getPlayerTycoon()
-if localPlayer.TycoonOwned and localPlayer.TycoonOwned.Value ~= nil then
-playerTycoon = localPlayer.TycoonOwned.Value
-end
+    if localPlayer.TycoonOwned and localPlayer.TycoonOwned.Value ~= nil then
+        playerTycoon = localPlayer.TycoonOwned.Value
+    end
 end
 
-local function autoClickOnMine()
-while autoClickerActive do
-if fireclickdetector and playerTycoon and playerTycoon:WaitForChild("Purchases").db1.Click then
-fireclickdetector(playerTycoon.Purchases.db1.Click.ClickDetector)
-end
-task.wait(autoClickerCooldown)
-end
+local function clickOnMine()
+    if fireclickdetector and playerTycoon and playerTycoon:WaitForChild("Purchases").db1.Click then
+        fireclickdetector(playerTycoon.Purchases.db1.Click.ClickDetector)
+    end
 end
 
 local function buyTycoonButtons()
-if not playerTycoon or not playerTycoon:FindFirstChild("Buttons") then
-return
+    if playerTycoon and playerTycoon:FindFirstChild("Buttons") then
+        for i, buttons in pairs(playerTycoon.Buttons:GetChildren()) do
+            if v.ClassName == "Model" and v:FindFirstChild("Button") and v:FindFirstChild("Price") then
+                if v.Button.Transparency == 0 and playerCash.Value >= v.Price.Value then
+                    if firetouchinterest then 
+                        firetouchinterest(playerHRP, v.Button, 0)
+                        firetouchinterest(playerHRP, v.Button, 1)
+                    end
+                end
+            end
+        end
+    end
 end
 
-for i, v in pairs(playerTycoon.Buttons:GetChildren()) do
-if not autoBuyTycoonButtonsActive then break end
-if v:IsA("Model") and v:FindFirstChild("Button") and v:FindFirstChild("Price") then
-local button = v.Button
-local price = v.Price
-if button.Transparency == 0 and playerCash.Value >= price.Value then
-if firetouchinterest then
-task.wait(math.random() * 0.2 + 0.1)
-if button and button.Parent and button.Transparency == 0 and playerCash.Value >= price.Value then
-firetouchinterest(playerHRP, button, 0)
-firetouchinterest(playerHRP, button, 1)
-end
-end
-end
-end
-end
-end
-
-local function autoCollectCash()
-if firetouchinterest then
-firetouchinterest(playerHRP, playerTycoon.MainItems.CashCollector.Button, 0)
-firetouchinterest(playerHRP, playerTycoon.MainItems.CashCollector.Button, 1)
-task.wait(autoCollectCashCooldown)
-end
+local function collectCash()
+    if firetouchinterest then
+        firetouchinterest(playerHRP, playerTycoon.MainItems.CashCollector.Button, 0)
+        firetouchinterest(playerHRP, playerTycoon.MainItems.CashCollector.Button, 1)
+    end
 end
 
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
@@ -148,34 +135,28 @@ local AutoClickerMineToggle = TycoonTab:CreateToggle({
     CurrentValue = false,
     Flag = "AutoClickerMineToggle",
     Callback = function(Value)
-    autoClickerActive = Value
-    if autoClickerActive == true then
-    task.spawn(autoClickOnMine)
+    settings.autoMineClicker = Value
+    if settings.autoMineClicker == true then
+        while settings.autoMineClicker then
+            clickOnMine()
+            task.wait(settings.autoClickerCooldown)
+        end
     end
     end,
 })
-
-local function autoBuyLoop()
-while autoBuyTycoonButtonsActive do
-buyTycoonButtons()
-task.wait(autoBuyTycoonButtonsCooldown)
-end
-end
-
-local function autoCollectLoop()
-while autoCollectCashActive do
-autoCollectCash()
-task.wait(autoCollectCashCooldown)
-end
-end
 
 local AutoBuyToggle = TycoonTab:CreateToggle({
     Name = "Auto Buy Tycoon Buttons",
     CurrentValue = false,
     Flag = "AutoBuyTycoonToggle",
     Callback = function(Value)
-    autoBuyTycoonButtonsActive = Value
-    if Value then task.spawn(autoBuyLoop) end
+    settings.autoBuyTycoonButtons = Value
+    if settings.autoBuyTycoonButtons == true then
+        while settings.autoBuyTycoonButtons then
+            buyTycoonButtons()
+            task.wait(settings.autoBuyTycoonButtonsCooldown)
+        end
+    end
     end,
 })
 
@@ -184,8 +165,13 @@ local AutoCollectToggle = TycoonTab:CreateToggle({
     CurrentValue = false,
     Flag = "AutoCollectCashToggle",
     Callback = function(Value)
-    autoCollectCashActive = Value
-    if Value then task.spawn(autoCollectLoop) end
+    settings.autoCollectCash = Value
+    if settings.autoCollectCash == true then
+        while settings.autoCollectCash then
+            collectCash()
+            task.wait(settings.autoCollectCashCooldown)
+        end
+    end
     end,
 })
 
@@ -194,42 +180,38 @@ local ClickerCooldownSlider = TycoonTab:CreateSlider({
     Range = {0.01, 1},
     Increment = 0.01,
     Suffix = "s",
-    CurrentValue = autoClickerCooldown,
+    CurrentValue = settings.autoClickerCooldown,
     Flag = "ClickerCooldown",
     Callback = function(Value)
-    autoClickerCooldown = Value
+    settings.autoClickerCooldown = Value
     end,
 })
 
 local BuyCooldownSlider = TycoonTab:CreateSlider({
     Name = "Buy Buttons Cooldown",
-    Range = {
-        0.01, 1
-    },
+    Range = {0.01, 1},
     Increment = 0.01,
     Suffix = "s",
-    CurrentValue = autoBuyTycoonButtonsCooldown,
+    CurrentValue = settings.autoBuyTycoonButtonsCooldown,
     Flag = "BuyCooldown",
     Callback = function(Value)
-    autoBuyTycoonButtonsCooldown = Value
+    settings.autoBuyTycoonButtonsCooldown = Value
     end,
 })
 
 local CollectCooldownSlider = TycoonTab:CreateSlider({
     Name = "Collect Cash Cooldown",
-    Range = {
-        0.01, 1
-    },
+    Range = {0.01, 1},
     Increment = 0.01,
     Suffix = "s",
-    CurrentValue = autoCollectCashCooldown,
+    CurrentValue = settings.autoCollectCashCooldown,
     Flag = "CollectCooldown",
     Callback = function(Value)
-    autoCollectCashCooldown = Value
+    settings.autoCollectCashCooldown = Value
     end,
 })
 
-local function updateHRP()
+local function updateHRP() --this is a mess deff
 local character = localPlayer.Character or localPlayer.CharacterAdded:Wait()
 playerHRP = character:WaitForChild("HumanoidRootPart")
 end
