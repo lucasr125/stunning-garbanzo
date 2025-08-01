6f212d4f7ff70dcb4a49f602236c242f62cbf329
lucasr125 <lucasr125348@gmail.com>
2025-08-01 10:58:29 -0300

    Ooops...

--- Loader.lua
+++ Loader.lua
@@ -7,7 +7,7 @@ elseif placeId = 12639388562 then --// Difficulty Tyc
 elseif placeId = 12779735953 then --// JJT Tyc
     loadstring(game:HttpGet("https://raw.githubusercontent.com/lucasr125/stunning-garbanzo/refs/heads/main/JjtTycoon.lua"))()
 elseif placeId = 123352919072485 then --// Ore Fac Tyc
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/lucasr125/stunning-garbanzo/refs/heads/main/MineFactoryTycoon.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lucasr125/stunning-garbanzo/refs/heads/main/OreFactoryTycoon.lua"))()
 elseif placeId = 12779735953 then --// Smoothie Fac Tyc
     loadstring(game:HttpGet("https://raw.githubusercontent.com/lucasr125/stunning-garbanzo/refs/heads/main/SmoothieFactoryTycoon.lua"))()
 end
