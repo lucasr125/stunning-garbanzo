1952edce55d8bc1076c12c8a08197353190d0c81
lucasr125 <lucasr125348@gmail.com>
2025-08-01 10:57:58 -0300

    Updated

---
+++ Loader.lua
@@ -0,0 +1,13 @@
+local placeId = game.PlaceId
+
+if placeId == 6915291292 then --// Bakery Sim
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lucasr125/stunning-garbanzo/refs/heads/main/BakerySimulator.lua"))()
+elseif placeId = 12639388562 then --// Difficulty Tyc
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lucasr125/stunning-garbanzo/refs/heads/main/DifficultyTycoon.lua"))()
+elseif placeId = 12779735953 then --// JJT Tyc
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lucasr125/stunning-garbanzo/refs/heads/main/JjtTycoon.lua"))()
+elseif placeId = 123352919072485 then --// Ore Fac Tyc
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lucasr125/stunning-garbanzo/refs/heads/main/MineFactoryTycoon.lua"))()
+elseif placeId = 12779735953 then --// Smoothie Fac Tyc
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lucasr125/stunning-garbanzo/refs/heads/main/SmoothieFactoryTycoon.lua"))()
+end
